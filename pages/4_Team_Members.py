2750d1428623b9057eaf01e5dbed5e14dd270028
Divyanshu Ranjan <123241944+divyanshu-17@users.noreply.github.com>
2025-08-02 10:01:11 +0530

    Update 4_Team_Members.py

--- pages/4_Team_Members.py
+++ pages/4_Team_Members.py
@@ -5,27 +5,28 @@ st.title("👨‍💻 Team Members")
 team = [
     {
         "name": "Divyanshu Ranjan",
-        "image": "D:\A Ingredient Waste App\pages\assets/member1",
-        "linkedin": "linkedin.com/in/divyanshu-ranjan-b23986271"
+        "image": "assets/member1.jpg",
+        "linkedin": "https://linkedin.com/in/divyanshu-ranjan-b23986271"
     },
     {
         "name": "Awanish Bhatt",
-        "image": "D:\A Ingredient Waste App\pages\assets/member1/bhatt",
-        "linkedin": "linkedin.com/in/awanish-bhatt-153585276"
+        "image": "assets/bhatt.jpeg",
+        "linkedin": "https://linkedin.com/in/awanish-bhatt-153585276"
     },
     {   
-            "name": "Harsh Awasthi",
-        "image": "linkedin.com/in/awanish-bhatt-153585276/aw",
-        "linkedin": "linkedin.com/in/harsh-awasthi-6132b82a1"
+        "name": "Harsh Awasthi",
+        "image": "assets/aw.jpeg",
+        "linkedin": "https://linkedin.com/in/harsh-awasthi-6132b82a1"
     },
     {
-            "name": "Akash Nigam",
-        "image": "linkedin.com/in/harsh-awasthi-6132b82a1/nigam",
-        "linkedin": "linkedin.com/in/akash-nigam-3436ab248"
+        "name": "Akash Nigam",
+        "image": "assets/nigam.jpeg",
+        "linkedin": "https://linkedin.com/in/akash-nigam-3436ab248"
     }
 ]
 
 cols = st.columns(len(team))
+
 for i, member in enumerate(team):
     with cols[i]:
         st.image(member["image"], caption=member["name"], width=150)
